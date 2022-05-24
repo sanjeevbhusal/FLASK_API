6137820247dacd7e4a3f9f0a66518f0cfbac6077
Sanjeev <bhusalsanjeev23@gmail.com>
2022-05-24 22:05:50 +0545

    Configured herokus prblm to connecting to postgres database

--- api/config.py
+++ api/config.py
@@ -1,7 +1,11 @@
 import secrets
 import os
 
+uri = os.getenv("DATABASE_URL") 
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
 
 class Config:
     SECRET_KEY = os.environ["SECRET_KEY"]
-    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
+    SQLALCHEMY_DATABASE_URI = uri
