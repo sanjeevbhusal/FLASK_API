e41571a3e171f9d3e6f44e20978c18992cb330d6
Sanjeev <bhusalsanjeev23@gmail.com>
2022-06-17 12:15:06 +0545

    Fixed create_new_post error

--- api/main/routes.py
+++ api/main/routes.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
+from flask import request
 
 main = Blueprint("main", __name__)
 
 @main.route("/")
 def home():
+    import pdb;pdb.set_trace()
     return {"message" : "Welcome to the Home Page"}
